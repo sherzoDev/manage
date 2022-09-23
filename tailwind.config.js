5d5cde388ed0a8cb000e1b8e743aa47344795a0a
sherzoDev <mustafoqulovsherzodbek@gmail.com>
2022-09-23 17:51:02 +0500

    Add Tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    fontFamily: {
+      body: ['"Be Vietnam Pro"']
+    }
+  },
+  plugins: [],
+}
